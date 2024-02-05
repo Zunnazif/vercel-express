a2ff5393c240b65349f614e44cea365a81e9854b
faizunnazif <faizoen75@gmail.com>
2024-02-05 10:09:15 +0700

    adj db

--- router/routerEmail.js
+++ router/routerEmail.js
@@ -1,52 +1,47 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
+import express from "express";
+import nodemailer from "nodemailer";
 
 const route = express.Router();
 
+route.post("/SendToEmail", (req, res) => {
+  const { nameFrom, sendFrom, password, nameTo, sendTo, subject, bodyHTML } =
+    req.body;
 
-route.post('/SendToEmail', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "zunna.digital47@gmail.com",
+      pass: "qjbo vdys vrfk vehp",
+    },
+    secure: true, // upgrades later with STARTTLS -- change this based on the PORT
+  });
 
-    const { 
-        nameFrom, 
-        sendFrom, 
-        password, 
-        nameTo, 
-        sendTo, 
-        subject, 
-        bodyHTML 
-    } = req.body;
-    
+  const mailData = {
+    from: {
+      name: nameFrom,
+      address: sendFrom,
+    },
+    to: {
+      name: nameTo,
+      address: sendTo,
+    },
+    subject: subject,
+    html: bodyHTML,
+  };
 
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: sendFrom,
-            pass: password,
-        },
-        secure: true, // upgrades later with STARTTLS -- change this based on the PORT
-    });
-    
-    const mailData = {
-        from: {
-            name: nameFrom,
-            address: sendFrom
-        },
-        to: {
-            name: nameTo,
-            address: sendTo
-        },
-        subject: subject,
-        html: bodyHTML,
-    };
+  transporter.sendMail(mailData, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    res
+      .status(200)
+      .send({
+        isSucceeded: true,
+        message: "Mail send",
+        response: info.envelope.to[0],
+      });
+  });
+});
 
-    transporter.sendMail(mailData, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        res.status(200).send({ isSucceeded: true, message: "Mail send", response: info.envelope.to[0]});
-        
-    });
-})
-
-export default route
+export default route;
